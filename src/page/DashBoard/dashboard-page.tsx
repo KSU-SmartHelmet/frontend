0a6a16426440a5ecb05b54eb39677b3bfacd9afc
wooki <hyunook02@naver.com>
2025-07-01 22:44:12 +0900

    feat(#4): 로그아웃 API 구현

--- src/page/DashBoard/dashboard-page.tsx
+++ src/page/DashBoard/dashboard-page.tsx
@@ -3,6 +3,10 @@ import Header from "../Header";
 import DashBoardMainContent from "./DashBoardMainContent";
 
 export function DashboardPage() {
+  const env = {
+    SERVER_URL: import.meta.env.VITE_SERVER_URL,
+  };
+
   const navigate = useNavigate();
   const location = useLocation();
   const isMapView = location.pathname === "/map";
@@ -11,9 +15,23 @@ export function DashboardPage() {
     navigate(showMap ? "/map" : "/dashboard");
   };
 
+  const onLogout = () => {
+    fetch(`${env.SERVER_URL}/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("accessToken"),
+      },
+    })
+      .then(() => {
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+      });
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header showMap={isMapView} setShowMap={handleViewChange} />
+      <Header showMap={isMapView} setShowMap={handleViewChange} onLogout={onLogout} />
       <DashBoardMainContent />
     </div>
   );
