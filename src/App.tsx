1a73d62e93a3dceedfc4b28bac97e3a77dacef57
wooki <hyunook02@naver.com>
2025-07-12 13:57:18 +0900

    feat(#4): 회원가입 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { LoginPage } from "./components/login-page";
 import { DashboardPage } from "./page/DashBoard/dashboard-page";
 import { MapView } from "./components/map-view";
+import { SignupPage } from "./components/signup-page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/map" element={<MapView />} />
         <Route path="/" element={<LoginPage />} />
